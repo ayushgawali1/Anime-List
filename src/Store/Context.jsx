381eeb8bed698fd39eb2ee335d1d312154be7e02
Ayush Gawali <142976942+ayushgawali1@users.noreply.github.com>
2024-09-14 17:12:50 +0530

    Add files via upload

---
+++ src/Store/Context.jsx
@@ -0,0 +1,17 @@
+import {createContext} from 'react';
+
+export const Context  = createContext({
+    value:null,
+    setvalue:null,
+    mode:null,
+    setMode:null,
+    search:null,
+    setSearch:null,
+    handleChange:null,
+    name:null,
+    setName:null,
+    searchSubmitButtonClick:null,
+    setSearchSubmitButtonClick:null,
+    listClickHandler:null,
+    setListClickHandler:null,
+})
